3b8587d61d9d262ae4eaf4d368f3b7d0bd27e804
akejolin <akejolin@gmail.com>
2020-11-26 02:42:59 +0100

    added lint and jest

--- plugin.js
+++ plugin.js
@@ -6,7 +6,7 @@ const defaultFileExt= ['.txt']
 function formatOptions(options) {
   const {
     exts = defaultFileExt,
-  } = options || {};
+  } = options || {}
 
   return {
     exts: Array.isArray(exts) && exts.length > 0 ? exts : defaultFileExt,
@@ -14,17 +14,17 @@ function formatOptions(options) {
 }
 
 module.exports = function (snowpackConfig, pluginOptions) {
-  const { exts } = formatOptions(pluginOptions);
+  const { exts } = formatOptions(pluginOptions)
   return {
     name: pkt.name,
     resolve: {input: exts, output: [".js"]},
     async load({filePath}) {
-      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      const fileContent = fs.readFileSync(filePath, 'utf-8')
       const json = JSON.stringify(fileContent)
         .replace(/\u2028/g, '\\u2028')
-        .replace(/\u2029/g, '\\u2029');
+        .replace(/\u2029/g, '\\u2029')
 
-      return `export default ${json};`;
+      return `export default ${json}`
 
     },
   }
